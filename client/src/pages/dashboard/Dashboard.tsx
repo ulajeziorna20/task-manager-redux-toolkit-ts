61f859c5991d5ce06af8cc5bfb4646429bb4c84c
Ula Jeziorna <jeziornaula@wp.pl>
2023-10-31 13:31:27 +0100

    fixes in the Dashboard subpage

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Link } from "react-router-dom";
 import Sidebar from "../../components/sidebar/Sidebar";
@@ -7,56 +7,64 @@ import './dashboard.scss'
 import { AppDispatch, RootState } from "../../slices/store";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllTasks } from '../../api/getAllTasks';
+import { initialTask } from '../../slices/taskSlice';
 
 const Dashboard = () => {
-
-  // console.log('montuje');
-  
-
-  const tasklist = useSelector((state: RootState) => state.task.AllTasks);
-  const currentUser = useSelector((state: RootState) => state.auth.currentUser);
-
-
-  let pendingTask = [];
-  let completedTask = [];
-
-  // console.log(tasklist.length);
-  
-
-
-  for (let i= 0; i < tasklist.length; i++) {
-
-
-    console.log(tasklist[i].status)
-    
-    if (tasklist[i].status === 'todo') {
-
-
-      pendingTask.push(tasklist[i]);
-    } else if (tasklist[i].status === 'done') {
-      completedTask.push(tasklist[i]);
-    }
-
-    // window.location.reload()
-  }
-
 
   const dispatch: AppDispatch = useDispatch();
 
 
-  // useEffect(() => {
-
-  //   // console.log(tasklist);
-    
-
-  //   if (currentUser !== undefined && currentUser !== null) {
-
-  //     dispatch(getAllTasks(currentUser.token, currentUser.id));
-  //   }
+  const currentUser = useSelector((state: RootState) => state.auth.currentUser);
+  const tasklist = useSelector((state: RootState) => state.task.AllTasks);
 
 
-  // }, [dispatch, currentUser?.token, currentUser?.id]);
 
+  let pendingTaskVar: initialTask[] = []
+  let completedTaskVar: initialTask[] = []
+
+  for (let i = 0; i < tasklist.length; i++) {
+
+    if (tasklist[i].status === 'todo') {
+
+      pendingTaskVar.push(pendingTaskVar[i]);
+    } else if (tasklist[i].status === 'done') {
+      completedTaskVar.push(completedTaskVar[i]);
+    }
+
+
+  }
+
+  console.log("tyle ma pending " + pendingTaskVar.length);
+
+
+
+  console.log(tasklist.length);
+
+
+  console.log('jestem drugi');
+
+
+
+
+
+  useEffect(() => {
+    console.log('jestem UE');
+
+    const fetchTasks = async () => {
+      if (currentUser?.token !== undefined && currentUser?.token !== null) {
+        await dispatch(getAllTasks(currentUser.token, currentUser.id));
+        // Tutaj możesz być pewien, że zadania zostały pobrane przed użyciem useSelector
+        console.log('Zadania zostały pobrane')
+
+      }
+    };
+
+    fetchTasks();
+
+
+
+
+  }, [currentUser?.token, currentUser?.id, dispatch]);
 
 
 
@@ -69,8 +77,9 @@ const Dashboard = () => {
         <div className="dashboard_rightContent">
           <h2>Task Status Dashboard</h2>
           <div className='taskcount'>
-            <div className='todo box'>Todo - {pendingTask.length}</div>
-            <div className='done box'>Complete - {completedTask.length}</div>
+
+            <div className='todo box'>Todo - {pendingTaskVar.length}</div>
+            <div className='done box'>Complete - {completedTaskVar.length}</div>
           </div>
           <div className="createButton">
             <Link to='/taskmanager' className="button">Task List and create</Link>
